f32bb23725c73a75012da95f37e08e6184b0e443
Akhil Pentamsetti <akhil.pab@gmail.com>
2026-06-20 16:14:37 +0530

    feat: add docs for tfa

--- astro.config.mjs
+++ astro.config.mjs
@@ -46,6 +46,7 @@ export default defineConfig({
 			sidebar: [
 				{ label: 'Getting Started', items: [{ autogenerate: { directory: 'getting-started' } }] },
 				{ label: 'Core Concepts', items: [{ autogenerate: { directory: 'concepts' } }] },
+				{ label: 'Account', items: [{ autogenerate: { directory: 'account' } }] },
 						{ label: 'Playwright', items: [{ autogenerate: { directory: 'reporter/playwright' } }] },
 					{ label: 'pytest', items: [{ autogenerate: { directory: 'reporter/pytest' } }] },
 					{ label: 'Vitest', items: [{ autogenerate: { directory: 'reporter/vitest' } }] },
